6de05ad05ebb491ee98083815904ccc13dd0fc89
Ariel Young <arielyoung103@gmail.com>
2020-03-28 00:15:07 -0700

    Updated requirements

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -9,4 +9,5 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
+
     return config.make_wsgi_app()
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
+from pyramid.request import Request
 
 
 @view_config(route_name='home', renderer='templates/mytemplate.jinja2')
-def my_view(request):
+def my_view(request: Request):
     return {'project': 'backend'}
