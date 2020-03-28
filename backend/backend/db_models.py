3111825fcb2ea7f3502bd7f2ddacec84b5959bbb
Ariel Young <arielyoung103@gmail.com>
2020-03-28 03:19:06 -0700

    More database stuff

--- backend/backend/db_models.py
+++ backend/backend/db_models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Integer, Column, Text, Boolean, ForeignKey, ARRAY, String
+from sqlalchemy import Integer, Column, Text, Boolean, ForeignKey, ARRAY, String(75)
 import from passlib import hash
 
 import enum
@@ -7,17 +7,17 @@ import enum
 Base = declarative_base()
 
 class _AbstractUser(Base):
-    username   = Column(Text, primary_key=True)
-    email      = Column(Text)
-    first_name = Column(Text)
-    last_name  = Column(Text)
+    username   = Column(String(75), primary_key=True)
+    email      = Column(String(75))
+    first_name = Column(String(75))
+    last_name  = Column(String(75))
 
-    geo_location_cntry = Column(Text)
-    geo_location_state = Column(Text)
-    geo_location_city  = Column(Text)
+    geo_location_cntry = Column(String(75))
+    geo_location_state = Column(String(75))
+    geo_location_city  = Column(String(75))
 
-    _password_hash = Column(Text)
-    _user_type = Column(Text)
+    _password_hash = Column(String(75))
+    _user_type = Column(String(75))
 
     __tablename__ = 'users'
     __mapper_args__ = {
@@ -25,11 +25,18 @@ class _AbstractUser(Base):
         'polymorphic_on': _user_type
     }
 
-    def __init__(self, uname: str, password: str, email: str, fname: str, lname: str):
-        self.username = uname
-        self.email = email
+    def __init__(self, uname: str, password: str, email: str, fname: str, lname: str, geo_country: str, geo_state: str,
+                 geo_city: str, user_type: str):
+        self.username    = uname
+        self.email       = email
         self.first_name  = fname
-        self.last_name = lname
+        self.last_name   = lname
+
+        self.geo_location_cntry = geo_country
+        self.geo_location_state = geo_state
+        self.geo_location_city  = geo_city
+
+        self._user_type = user_type
 
         self._password_hash = hash.pbkdf2_sha256.hash(password)
 
@@ -42,12 +49,21 @@ class _AbstractUser(Base):
                                                                             email=self.email, usert=self._user_type)
 
 class DoctorUser(_AbstractUser):
-    username      = Column(Text, ForeignKey('base_user.username'), primary_key=True)
-    hospital      = Column(Text)
+    username      = Column(String(75), ForeignKey('base_user.username'), primary_key=True)
+    hospital      = Column(String(75))
 
-    alma_mater     = Column(Text)
-    specialization = Column(Text)
+    alma_mater     = Column(String(75))
+    specialization = Column(String(75))
     biography      = Column(Text)
+
+    def __init__(self, uname: str, password: str, email: str, fname: str, lname: str, geo_country: str,
+                 geo_state: str, geo_city: str, hospital: str, alma_mater: str, specialization: str, biography: str):
+        self.hospital = hospital
+        self.alma_mater = alma_mater
+        self.specialization = specialization
+        self.biography = biography
+
+        super().__init__(uname, password, email, fname, lname, geo_country, geo_state, geo_city, "Doctor")
 
     __tablename__ = "doctors"
     __mapper_args__ = {
@@ -59,18 +75,27 @@ class FabUser(_AbstractUser):
     username = Column(Text, ForeignKey('base_user.username'), primary_key=True)
     hospital = Column(Text)
 
-    printer_model = Column(Text)
-    filament_capable = Column(ARRAY(String))
+    printer_model         = Column(Text)
+    filament_capable      = Column(ARRAY(String(10)))
+    print_quality_capable = Column(Integer) # This should be a number out of 10
 
+    def __init__(self, uname: str, password: str, email: str, fname: str, lname: str, geo_country: str,
+                 geo_state: str, geo_city: str, printer_model: str, filament_capable: list, print_quality_capable: str):
+        self.printer_model         = printer_model
+        self.filament_capable      = filament_capable
+        self.print_quality_capable = print_quality_capable
 
-    __tablename__ = "doctors"
+        super().__init__(uname, password, email, fname, lname, geo_country, geo_state, geo_city, "Fabricator")
+
+    __tablename__ = "fabricators"
     __mapper_args__ = {
-        'polymorphic_identity': 'doctor'
+        'polymorphic_identity': 'fabricator'
     }
 
 
-class FabUser(_AbstractUser):
+class _AbstractPost():
+    title = Column(String(75))
+
+
+class PrintRequest:
     pass
-
-
-class PrintRequest
